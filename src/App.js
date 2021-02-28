a22f2d1cdb5349288c04a3f0677f184371797efc
aszegedy <andras.szegedy@gmail.com>
2021-02-28 16:35:02 +0100

    5. new/upcoming init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { loadGames } from "./actions/gamesAction";
+import Home from "./pages/Home";
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadGames());
-  });
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
